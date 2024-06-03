f42ca0ccb8559aacafcd3d8e9f60330769d2cd55
Lam <chin-keong.lam@hp.com>
2024-06-03 15:24:04 -0700

    first commit

---
+++ ci.sh
@@ -0,0 +1,7 @@
+echo "# xiaomictest" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:cklam12345/xiaomictest.git
+git push -u origin main
